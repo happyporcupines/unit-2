4687c8ed11fac141e13762649076aadcfd7db092
happyporcupines <skibble11@gmail.com>
2026-02-18 19:07:53 -0600

    adding aspects to map

---
+++ js/quickstartTutorial.js
@@ -0,0 +1,22 @@
+// initialize the view set at London at an appropriate zoom level
+var map = L.map('map').setView([51.505, -0.09], 13);
+// adding open street map tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+// adding a marker
+var marker = L.marker([51.5, -0.09]).addTo(map);
+// adding a circle
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+// adding a polygon
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
