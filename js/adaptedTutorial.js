effee8f76c896fe8480a0f3904e720231ceb1585
happyporcupines <skibble11@gmail.com>
2026-02-19 10:42:10 -0600

    marker popup fix 3

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -18,18 +18,15 @@ function createMap(){
     //call getData function
     getData(map);
 };
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
+function createPopupContent(properties) {
     var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+    if (properties) {
+        for (var property in properties) {
+            popupContent += "<p>" + property + ": " + properties[property] + "</p>";
         }
-        layer.bindPopup(popupContent);
-    };
-};
+    }
+    return popupContent;
+}
 
 //function to retrieve the data and place it on the map
 function getData(map){
@@ -49,9 +46,10 @@ function getData(map){
 
             //create a Leaflet GeoJSON layer and add it to the map
             L.geoJson(json, {
-                onEachFeature: onEachFeature,
                 pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                    var layer = L.circleMarker(latlng, geojsonMarkerOptions);
+                    layer.bindPopup(createPopupContent(feature.properties));
+                    return layer;
                 }
             }).addTo(map);
         })
