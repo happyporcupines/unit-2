454258e1ffc369d2456e834806e4fb92a8884365
happyporcupines <skibble11@gmail.com>
2026-02-19 10:21:49 -0600

    added circle markers and popups

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -18,19 +18,46 @@ function createMap(){
     }).addTo(map);
 
     //call getData function
-    getData();
+    getData(map);
+};
+
+//added at Example 2.3 line 20...function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
 };
 
 //function to retrieve the data and place it on the map
-function getData(){
+function getData(map){
     //load the data
     fetch("data/MegaCities.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
+            L.geoJson(json, {
+                onEachFeature: onEachFeature,
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
         })
 };
 
